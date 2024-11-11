25f672417bea28708e81f6ba661b6c2b118e6a0e
Abhishek Dhar <112891795+abhishekdhar999@users.noreply.github.com>
2024-11-11 22:54:21 +0530

    Update user.model.js

--- Model/user.model.js
+++ Model/user.model.js
@@ -1,5 +1,5 @@
 import mongoose , {Schema} from "mongoose";
-import bcrypt from 'bcrypt';
+
 import jwt from 'jsonwebtoken'
 const userSchema = new Schema(
     {
@@ -64,18 +64,8 @@ const userSchema = new Schema(
 
 userSchema.index({ location: '2dsphere' });
 
-userSchema.pre('save',async function(next){
-    if(!this.isModified("password")){
-        return next();
-    }
 
-    this.password = bcrypt.hash(this.password,10);
-    next();
-})
 
-userSchema.methods.isPasswordCorrect = async function (password){
-    return await bcrypt.compare(password,this.password);
-}
 
 userSchema.methods.generateAccessTokken = async function (){
 return jwt.sign(
