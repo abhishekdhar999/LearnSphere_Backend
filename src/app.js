8ec89a2ec6323dd1b67a9d8f988463250cb2d55a
Abhishek Dhar <dhar.abhishek999@gmail.com>
2024-11-20 19:32:23 +0530

    change

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 // routes
+import cors from 'cors'
 import authRoutes from '../Routes/authRoutes.js';
 import videoRoutes from '../Routes/videoRoutes.js'
 
@@ -13,7 +14,11 @@ import communityRoutes from '../Routes/communityRoutes.js'
 const app = express();
 
  
-app.use(cors());
+app.use(cors(
+    {
+        origin: ['https://learn-sphere-frontend-9omx4f33z-abhishek-dhars-projects.vercel.app/','http://localhost:3000', 'http://localhost:3001'],
+    }
+));
 app.use(express.json({limit:"16kb"}))
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
@@ -22,7 +27,7 @@ app.use(express.static('public'));
 
 
 
-// routes
+
 
 
  app.use("/users",authRoutes);
--- src/index.js
+++ src/index.js
@@ -17,58 +17,99 @@ import {Server} from "socket.io"
 
 
 const port = process.env.PORT || 10000;
- const host = '0.0.0.0';
-const server =   app.listen(port, host, () => {
-  console.log(`Server running on http://${host}:${port}`);
+//  const host = '0.0.0.0';
+const server =   app.listen(port, () => {
+  console.log(`Server running on :${port}`);
 });
 //  console.log("server",server)
-const io = new Server(server, {
-  cors:{
-    origin: "*",
-    methods: ["GET", "POST"],
-    credentials:true
-  }
-  // cors: {
-  //   origin: "https://learn-sphere-frontend.vercel.app", // Allow frontend domain
-  //   // methods: ["GET", "POST"],
-  //   credentials: true,
-  // },
-});
-// app.use(express.static(path.join(__dirname, 'public')));
+// const io = new Server(server, {
+//   cors:{
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//     credentials:true
+//   }
+//   // cors: {
+//   //   origin: "https://learn-sphere-frontend.vercel.app", // Allow frontend domain
+//   //   // methods: ["GET", "POST"],
+//   //   credentials: true,
+//   // },
+// });
+// // app.use(express.static(path.join(__dirname, 'public')));
 
 
-// Handle the root route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// If you're serving a frontend, make sure to serve the index.html (React app, etc.)
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// // Handle the root route
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
 // });
 
+// // If you're serving a frontend, make sure to serve the index.html (React app, etc.)
+// // app.get('*', (req, res) => {
+// //   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// // });
 
 
-// const wsServer = new WebSocketServer({ server });
-// const userSockets = new Map(); // Map userId -> WebSocket connection
 
-// wsServer.on("connection", function connection(ws) {
-//   console.log("Connected to WebSocket");
+// // const wsServer = new WebSocketServer({ server });
+// // const userSockets = new Map(); // Map userId -> WebSocket connection
 
-//   userSockets.set('6707ad467f624c0eed660873', ws)
+// // wsServer.on("connection", function connection(ws) {
+// //   console.log("Connected to WebSocket");
 
-//   ws.on('message', async (data) => {
-//     const messageString = data.toString();
+// //   userSockets.set('6707ad467f624c0eed660873', ws)
+
+// //   ws.on('message', async (data) => {
+// //     const messageString = data.toString();
+// //     const parsedData = JSON.parse(messageString); 
+// //     const { chatId, sender, content, isGroupChat, type, sdp, candidate, communityId } = parsedData;
+
+// //     // Handle WebRTC signaling for video calls
+// //     if (type === "video-offer" || type === "video-answer" || type === "new-ice-candidate") {
+// //       handleWebRTCSignaling(parsedData, ws);
+// //       return;
+// //     }
+
+// //     // Save the message to the database
+// //     const newMessage = new Message({ chatId, sender, content });
+// //     await newMessage.save();
+
+// //     // Broadcast message in group or direct chat
+// //     if (isGroupChat) {
+// //       const groupChat = await GroupChat.findById(chatId).populate('participants', 'name email _id');
+// //       if (!groupChat) {
+// //         console.error('Group chat not found');
+// //         return;
+// //       }
+
+// //       groupChat.participants.forEach(participant => {
+// //         const participantSocket = userSockets.get(participant._id.toString());
+// //         if (participantSocket && participantSocket.readyState === WebSocket.OPEN) {
+// //           participantSocket.send(JSON.stringify({ chatId, sender, content }));
+// //         }
+// //       });
+// //     } else {
+// //       wsServer.clients.forEach(client => {
+// //         if (client !== ws && client.readyState === WebSocket.OPEN) {
+// //           client.send(JSON.stringify({ chatId, sender, content }));
+// //         }
+// //       });
+// //     }
+// //   });
+
+// //   ws.on("error", (err) => {
+// //     console.error("Socket error:", err);
+// //   });
+// // });
+// 250311023590
+// io.on('connection', (socket) => {
+//   console.log('A user connected:', socket.id);
+
+//   // Listen for messages
+//   socket.on('sendMessage',async (message) => {
+//     console.log('Message received:', message);
+//      const messageString = message.toString();
 //     const parsedData = JSON.parse(messageString); 
 //     const { chatId, sender, content, isGroupChat, type, sdp, candidate, communityId } = parsedData;
 
-//     // Handle WebRTC signaling for video calls
-//     if (type === "video-offer" || type === "video-answer" || type === "new-ice-candidate") {
-//       handleWebRTCSignaling(parsedData, ws);
-//       return;
-//     }
-
-//     // Save the message to the database
 //     const newMessage = new Message({ chatId, sender, content });
 //     await newMessage.save();
 
@@ -80,192 +121,77 @@ app.get('/', (req, res) => {
 //         return;
 //       }
 
-//       groupChat.participants.forEach(participant => {
-//         const participantSocket = userSockets.get(participant._id.toString());
-//         if (participantSocket && participantSocket.readyState === WebSocket.OPEN) {
-//           participantSocket.send(JSON.stringify({ chatId, sender, content }));
+//       groupChat.participants.forEach((participant) => {
+//         const participantSocketId = userSockets.get(participant._id.toString());
+//         if (participantSocketId) {
+//           io.to(participantSocketId).emit("message", { chatId, sender, content });
 //         }
 //       });
+//       // groupChat.participants.forEach(participant => {
+//       //   const participantSocket = userSockets.get(participant._id.toString());
+//       //   if (participantSocket && participantSocket.readyState === WebSocket.OPEN) {
+//       //     participantSocket.send(JSON.stringify({ chatId, sender, content }));
+//       //   }
+//       // }
+//       // );
 //     } else {
-//       wsServer.clients.forEach(client => {
-//         if (client !== ws && client.readyState === WebSocket.OPEN) {
-//           client.send(JSON.stringify({ chatId, sender, content }));
+//       io.sockets.sockets.forEach(client => {
+//         if (client.id !== socket.id) { // Exclude the sender
+//           client.emit("message", { chatId, sender, content });
 //         }
+//         // if (client !== ws && client.readyState === WebSocket.OPEN) {
+//         //   client.send(JSON.stringify({ chatId, sender, content }));
+//         // }
 //       });
 //     }
+
+//     // Broadcast message to all clients
+//     io.emit('receiveMessage', message);
 //   });
 
-//   ws.on("error", (err) => {
-//     console.error("Socket error:", err);
+//   // Handle disconnection
+//   socket.on('disconnect', () => {
+//     console.log('User disconnected:', socket.id);
 //   });
 // });
-250311023590
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
 
-  // Listen for messages
-  socket.on('sendMessage',async (message) => {
-    console.log('Message received:', message);
-     const messageString = message.toString();
-    const parsedData = JSON.parse(messageString); 
-    const { chatId, sender, content, isGroupChat, type, sdp, candidate, communityId } = parsedData;
+// const emailToSocketIdMap = new Map();
+// const socketidToEmailMap = new Map();
 
-    const newMessage = new Message({ chatId, sender, content });
-    await newMessage.save();
-
-    // Broadcast message in group or direct chat
-    if (isGroupChat) {
-      const groupChat = await GroupChat.findById(chatId).populate('participants', 'name email _id');
-      if (!groupChat) {
-        console.error('Group chat not found');
-        return;
-      }
-
-      groupChat.participants.forEach((participant) => {
-        const participantSocketId = userSockets.get(participant._id.toString());
-        if (participantSocketId) {
-          io.to(participantSocketId).emit("message", { chatId, sender, content });
-        }
-      });
-      // groupChat.participants.forEach(participant => {
-      //   const participantSocket = userSockets.get(participant._id.toString());
-      //   if (participantSocket && participantSocket.readyState === WebSocket.OPEN) {
-      //     participantSocket.send(JSON.stringify({ chatId, sender, content }));
-      //   }
-      // }
-      // );
-    } else {
-      io.sockets.sockets.forEach(client => {
-        if (client.id !== socket.id) { // Exclude the sender
-          client.emit("message", { chatId, sender, content });
-        }
-        // if (client !== ws && client.readyState === WebSocket.OPEN) {
-        //   client.send(JSON.stringify({ chatId, sender, content }));
-        // }
-      });
-    }
-
-    // Broadcast message to all clients
-    io.emit('receiveMessage', message);
-  });
-
-  // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
-  });
-});
-
-const emailToSocketIdMap = new Map();
-const socketidToEmailMap = new Map();
-
-io.on("connection", (socket) => {
-  console.log(`Socket Connected`, socket.id);
-  socket.on("room:join", (data) => {
-    console.log("room:join", data);
-    const { email, room } = data;
-    emailToSocketIdMap.set(email, socket.id);
-    socketidToEmailMap.set(socket.id, email);
-    io.to(room).emit("user:joined", { email, id: socket.id });
-    socket.join(room);
-    io.to(socket.id).emit("room:join", data);
-  });
-
-  socket.on("user:call", ({ to, offer }) => {
-    io.to(to).emit("incomming:call", { from: socket.id, offer });
-  });
-
-  socket.on("call:accepted", ({ to, ans }) => {
-    io.to(to).emit("call:accepted", { from: socket.id, ans });
-  });
-
-  socket.on("peer:nego:needed", ({ to, offer }) => {
-    console.log("peer:nego:needed", offer);
-    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
-  });
-
-  socket.on("peer:nego:done", ({ to, ans }) => {
-    console.log("peer:nego:done", ans);
-    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
-  });
-});
-
-
-
-
-
-
-
-// import connectDB from "./db/db.js";
-// import { app } from "./app.js";
-// import dotenv from 'dotenv';
-// import { WebSocketServer, WebSocket } from 'ws';
-// import Message from "../Model/message.model.js";
-// import { GroupChat } from "../Model/groupChat.model.js";
-
-// const port = 3001;
-// dotenv.config();
-// connectDB();
-
-// const server = app.listen(port, () => {
-//   console.log("server is running on port", port);
-// });
-
-// console.log("server", server);
-
-// const wsServer = new WebSocketServer({
-//   server: server
-// });
-
-// // Map to store connected user sockets
-// const userSockets = new Map();
-
-// wsServer.on("connection", function connection(ws) {
-//   console.log("Connected to socket");
-
-//   // Handle message events from the client (text chat logic)
-//   ws.on('message', async (data) => {
-//     const parsedData = JSON.parse(data);
-//     const { chatId, sender, content, isGroupChat, type } = parsedData;
-
-//     // Handle video call signaling messages
-//     if (type === "video-offer" || type === "video-answer" || type === "new-ice-candidate") {
-//       handleWebRTCSignaling(parsedData, ws);
-//       return;
-//     }
-
-//     // Handle chat messages (existing logic)
-//     if (type === "chat-message") {
-//       // Step 2: Save the message to the database
-//       const newMessage = new Message({ chatId, sender, content });
-//       await newMessage.save();
-
-//       if (isGroupChat) {
-//         // Group Chat Logic
-//         const groupChat = await GroupChat.findById(chatId).populate('participants', 'name email _id');
-//         if (!groupChat) {
-//           console.error('Group chat not found');
-//           return;
-//         }
-
-//         // Step 3: Broadcast the message to all participants in the group
-//         groupChat.participants.forEach(participant => {
-//           const participantSocket = userSockets.get(participant._id.toString());
-//           if (participantSocket && participantSocket.readyState === WebSocket.OPEN) {
-//             participantSocket.send(JSON.stringify({ chatId, sender, content, type: "chat-message" }));
-//           }
-//         });
-//       } else {
-//         wsServer.clients.forEach(client => {
-//           if (client !== ws && client.readyState === WebSocket.OPEN) {
-//             client.send(JSON.stringify({ chatId, sender, content, type: "chat-message" }));
-//           }
-//         });
-//       }
-//     }
+// io.on("connection", (socket) => {
+//   console.log(`Socket Connected`, socket.id);
+//   socket.on("room:join", (data) => {
+//     console.log("room:join", data);
+//     const { email, room } = data;
+//     emailToSocketIdMap.set(email, socket.id);
+//     socketidToEmailMap.set(socket.id, email);
+//     io.to(room).emit("user:joined", { email, id: socket.id });
+//     socket.join(room);
+//     io.to(socket.id).emit("room:join", data);
 //   });
 
-//   // Handle errors (optional but useful)
-//   ws.on("error", (err) => {
-//     console.error("Socket error:", err);
+//   socket.on("user:call", ({ to, offer }) => {
+//     io.to(to).emit("incomming:call", { from: socket.id, offer });
+//   });
+
+//   socket.on("call:accepted", ({ to, ans }) => {
+//     io.to(to).emit("call:accepted", { from: socket.id, ans });
+//   });
+
+//   socket.on("peer:nego:needed", ({ to, offer }) => {
+//     console.log("peer:nego:needed", offer);
+//     io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+//   });
+
+//   socket.on("peer:nego:done", ({ to, ans }) => {
+//     console.log("peer:nego:done", ans);
+//     io.to(to).emit("peer:nego:final", { from: socket.id, ans });
 //   });
 // });
+
+
+
+
+
+
+
